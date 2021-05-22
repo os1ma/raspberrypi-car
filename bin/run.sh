21ea9af61e9af74db9ed997158258d13fee90aa4
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2021-05-22 19:04:15 +0900

    実行用スクリプトを追加

---
+++ bin/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o xtrace
+
+readonly SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd)"
+readonly PROJECT_HOME="${SCRIPT_DIR}/.."
+
+cd "${PROJECT_HOME}"
+rm -rf dist
+yarn tsc
+sudo node dist/index.js
