50e556496ddacf415b25d4873176a0945974929d
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2021-06-12 16:05:51 +0900

    コントローラによらずエンドポイントのファイルを統一

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,31 @@
 import RaspberryPiCarApplication from './application/raspberryPiCarApplication'
 import PWMMotorCarFactory from './infrastructure/rpio/pwmMotorCarFactory'
 import logger from './logger'
+import KeyboardController from './presentation/keyboard/keyboardController'
 import WebSocketController from './presentation/websocket/webSocketController'
 
-// TODO keyboardCarApplication.ts と異なる箇所以外を共通化する
+const argv = process.argv
+
+console.log(`Starting... argv = ${argv}`)
+
+function initializeController() {
+  if (argv.length <= 2) {
+    throw new Error(`argv.length is invalid. ${argv.length}`)
+  }
+
+  switch (argv[2]) {
+    case 'websocket':
+      return new WebSocketController()
+    case 'keyboard':
+      return new KeyboardController()
+    default:
+      throw new Error(`argv[2] is invalid. argv[2] = ${argv[2]}`)
+  }
+}
 
 const STOP_SIGNALS = ['SIGTERM', 'SIGINT']
 
-const controller = new WebSocketController()
+const controller = initializeController()
 const carFactory = new PWMMotorCarFactory()
 
 const app = new RaspberryPiCarApplication(controller, carFactory)
